4f18eea0240822b23060f96b722ac8d1160dbc66
Daniel Vega <98dvega@gmail.com>
2021-03-19 16:29:25 -0500

    Visual and functionalities of the audio player completed

--- player.js
+++ player.js
@@ -73,7 +73,7 @@ volumeSlider.addEventListener('input', (e) => {
 
 /* Implementation of the functionality of the audio player */
 
-const audio = document.querySelector('audio');
+var audio = document.querySelector('audio');
 const durationContainer = document.getElementById('duration');
 const currentTimeContainer = document.getElementById('current-time');
 const outputContainer = document.getElementById('volume-output');
@@ -94,8 +94,8 @@ const setSliderMax = () => {
     seekSlider.max = Math.floor(audio.duration);
 }
 
-const displayBufferedAmount = () => {
-    const bufferedAmount = Math.floor(audio.buffered.end(audio.buffered.length - 1));
+ const displayBufferedAmount = () => {
+    var bufferedAmount = Math.floor(audio.buffered.end(audio.buffered.length - 1));
     audioPlayerContainer.style.setProperty('--buffered-width', `${(bufferedAmount / seekSlider.max) * 100}%`);
 }
 
@@ -211,16 +211,84 @@ if('mediaSession' in navigator) {
     });
 }
 
-//Events and functionalities on images
-var title = document.querySelector("#plays"); //This is the title of the audio player
-var disk = document.querySelector("#audioPlayer"); //This is the audio player feature
+//Events and functionalities of the images
+let title = document.querySelector("#plays"); //This is the title of the audio player
+var count = 0;
+var first = 1;
+//Randomize the playlist with a random index
+// const random = (min,max)=>{
+//     return Math.round(Math.random()*(max-min) + min);
+// }
+const x = (arr)=>{
+    
+    if (first == 0){
+        first = 1;
+        count++;
+    }else{
+        if (count == (arr.length-1)){
+            count=0;
+        }else{
+            count++;
+        }
+    }
 
+    let miau = arr[count];
+    return miau;
+}
+const y = (arr) => {
+    
+    if (first == 1){
+        first = 0;
+        count--;
+        if (count == -1){
+            count=(arr.length-1);
+        }
+    }else{
+        if (count == 0){
+            count=(arr.length-1);
+        }else{
+            count--;
+        }
+    } 
+
+    let miau = arr[count];
+    return miau;    
+}
+
+
+//EPIC PLAYLIST ARRAY
+const epicPlaylist = [
+    "/playlists/epic/Armored Titan - Shingeki no Kyoji.mp3",
+    "/playlists/epic/Kara Theme - Detroit Become Human.mp3",
+    "/playlists/epic/The Opened Way - Shadow of the Colossus.mp3",
+    "/playlists/epic/Ultra Instinct - Dragon Ball Super.mp3",
+];
+
+//REFLEX PLAYLIST ARRAY
+const reflexPlaylist = [
+    "/playlists/reflex/Death Note - Main Theme.mp3",
+    "/playlists/reflex/Final Fantasy - Prelude (Orchestral).mp3",
+    "/playlists/reflex/Floating Museum - Ghost In The Shell.mp3",
+    "/playlists/reflex/Gwyn, Lord of Cinder - Dark Souls.mp3",
+];
+
+
+//COOL PLAYLIST ARRAY
+const coolPlaylist = [
+    "/playlists/cool/Pearl Jam - Black.mp3",
+    "/playlists/cool/higurashi no naku koro ni opening full.mp3",
+    "/playlists/cool/Naruto Ending Theme - Wind by Akeboshi.mp3",
+    "/playlists/cool/Naruto Hinata Bicycle ending xd.mp3",
+];
+
+//Epic playlist (fire button)
 var epic = document.querySelector("#epic");
 const fire = (e)=>{e.target.src="/img/epicOn.png"};
 const fireOff = (e)=>{e.target.src="/img/epic.png"};
 epic.addEventListener("mouseover",fire);
 epic.addEventListener("mouseout",fireOff);
-epic.addEventListener("click",(e)=>{
+//To tune the playlist name and the lights up
+epic.addEventListener("click",(e)=>{ 
     e.target.src="img/epicOn.png";
     mind.src="img/mind.png";
     cool.src="img/cool.png";
@@ -228,15 +296,78 @@ epic.addEventListener("click",(e)=>{
     mind.addEventListener("mouseout",brainOff);
     cool.addEventListener("mouseout",chillOff);
     title.textContent = `epic playlist`;
-    disk.src="/playlists/epic/Ultra Instinct - Dragon Ball Super.mp3";
-    disk.play();
 });
+epic.addEventListener("contextmenu",(e)=>{ 
+    e.target.src="img/epicOn.png";
+    mind.src="img/mind.png";
+    cool.src="img/cool.png";
+    epic.removeEventListener("mouseout",fireOff);
+    mind.addEventListener("mouseout",brainOff);
+    cool.addEventListener("mouseout",chillOff);
+    title.textContent = `epic playlist`;
+});
+//Functions for each image/button (and bound its playlist)
+epic.addEventListener('click', () => {
+    //Changing the song
+    audio.pause();
+    audio = new Audio(x(epicPlaylist));
+    audio.load();
+    //For the animation stuff
+    playAnimation.playSegments([14, 27], true);
+    requestAnimationFrame(whilePlaying);
+    playState = 'pause';
+    cancelAnimationFrame(raf);
+    //here we will run a hack so the browser will always load the audio and run the code xd
+    if (audio.readyState > 0) {
+        displayDuration();
+        setSliderMax();
+        displayBufferedAmount();
+        audio.play();
+    } else {
+        audio.addEventListener('loadedmetadata', () => {
+            displayDuration();
+            setSliderMax();
+            displayBufferedAmount();
+            audio.play();
+        });
+    }
+});
+epic.oncontextmenu = ()=>{
+    //Changing the song
+    audio.pause();
+    audio = new Audio(y(epicPlaylist));
+    audio.load();
+    //For the animation stuff
+    playAnimation.playSegments([14, 27], true);
+    requestAnimationFrame(whilePlaying);
+    playState = 'pause';
+    cancelAnimationFrame(raf);
+    //acá usamos un hack para que siempre nos ejecute duración
+    if (audio.readyState > 0) {
+        displayDuration();
+        setSliderMax();
+        displayBufferedAmount();
+        audio.play();
+    } else {
+        audio.addEventListener('loadedmetadata', () => {
+            displayDuration();
+            setSliderMax();
+            displayBufferedAmount();
+            audio.play();
+        });
+    };
+    return false;
+};
 
+
+
+//Chill playlist (mind button)
 var mind = document.querySelector("#mind");
 const brain = (e)=>{e.target.src="/img/mindOn.png"};
 const brainOff = (e)=>{e.target.src="/img/mind.png"};
 mind.addEventListener("mouseover",brain);
 mind.addEventListener("mouseout",brainOff);
+//To tune the playlist name and the lights up
 mind.addEventListener("click",(e)=>{
     e.target.src="img/mindOn.png";
     epic.src="img/epic.png";
@@ -244,14 +375,78 @@ mind.addEventListener("click",(e)=>{
     mind.removeEventListener("mouseout",brainOff);
     epic.addEventListener("mouseout",fireOff);
     cool.addEventListener("mouseout",chillOff);
-    title.textContent = `chill playlist`;
+    title.textContent = `reflex playlist`;
 });
+mind.addEventListener("contextmenu",(e)=>{
+    e.target.src="img/mindOn.png";
+    epic.src="img/epic.png";
+    cool.src="img/cool.png";
+    mind.removeEventListener("mouseout",brainOff);
+    epic.addEventListener("mouseout",fireOff);
+    cool.addEventListener("mouseout",chillOff);
+    title.textContent = `reflex playlist`;
+});
+//Functions for each image/button (and bound its playlist)
+mind.addEventListener('click', () => {
+    //Changing the song
+    audio.pause();
+    audio = new Audio(x(reflexPlaylist));
+    audio.load();
+    //For the animation stuff
+    playAnimation.playSegments([14, 27], true);
+    requestAnimationFrame(whilePlaying);
+    playState = 'pause';
+    cancelAnimationFrame(raf);
+    //acá usamos un hack para que siempre nos ejecute duración
+    if (audio.readyState > 0) {
+        displayDuration();
+        setSliderMax();
+        displayBufferedAmount();
+        audio.play();
+    } else {
+        audio.addEventListener('loadedmetadata', () => {
+            displayDuration();
+            setSliderMax();
+            displayBufferedAmount();
+            audio.play();
+        });
+    }
+});
+mind.oncontextmenu = ()=>{
+    //Changing the song
+    audio.pause();
+    audio = new Audio(y(reflexPlaylist));
+    audio.load();
+    //For the animation stuff
+    playAnimation.playSegments([14, 27], true);
+    requestAnimationFrame(whilePlaying);
+    playState = 'pause';
+    cancelAnimationFrame(raf);
+    //acá usamos un hack para que siempre nos ejecute duración
+    if (audio.readyState > 0) {
+        displayDuration();
+        setSliderMax();
+        displayBufferedAmount();
+        audio.play();
+    } else {
+        audio.addEventListener('loadedmetadata', () => {
+            displayDuration();
+            setSliderMax();
+            displayBufferedAmount();
+            audio.play();
+        });
+    };
+    return false;
+};
 
+
+//Cool playlist (sunglasses button)
 var cool = document.querySelector("#cool");
 const chill = (e)=>{e.target.src="/img/coolOn.png"};
 const chillOff = (e)=>{e.target.src="/img/cool.png"};
 cool.addEventListener("mouseover",chill);
 cool.addEventListener("mouseout",chillOff);
+//To tune the playlist name and the lights up
 cool.addEventListener("click",(e)=>{
     e.target.src="img/coolOn.png";
     mind.src="img/mind.png";
@@ -261,3 +456,67 @@ cool.addEventListener("click",(e)=>{
     epic.addEventListener("mouseout",fireOff);
     title.textContent = `cool playlist`;
 });
+cool.addEventListener("contextmenu",(e)=>{
+    e.target.src="img/coolOn.png";
+    mind.src="img/mind.png";
+    epic.src="img/epic.png";
+    cool.removeEventListener("mouseout",chillOff);
+    mind.addEventListener("mouseout",brainOff);
+    epic.addEventListener("mouseout",fireOff);
+    title.textContent = `cool playlist`;
+});
+//Functions for each image/button (and bound its playlist)
+cool.addEventListener('click', () => {
+    //Changing the song
+    audio.pause();
+    audio = new Audio(x(coolPlaylist));
+    audio.load();
+    //For the animation stuff
+    playAnimation.playSegments([14, 27], true);
+    requestAnimationFrame(whilePlaying);
+    playState = 'pause';
+    cancelAnimationFrame(raf);
+    //acá usamos un hack para que siempre nos ejecute duración
+    if (audio.readyState > 0) {
+        displayDuration();
+        setSliderMax();
+        displayBufferedAmount();
+        audio.play();
+    } else {
+        audio.addEventListener('loadedmetadata', () => {
+            displayDuration();
+            setSliderMax();
+            displayBufferedAmount();
+            audio.play();
+        });
+    }
+});
+cool.oncontextmenu = ()=>{
+    //Changing the song
+    audio.pause();
+    audio = new Audio(y(coolPlaylist));
+    audio.load();
+    //For the animation stuff
+    playAnimation.playSegments([14, 27], true);
+    requestAnimationFrame(whilePlaying);
+    playState = 'pause';
+    cancelAnimationFrame(raf);
+    //acá usamos un hack para que siempre nos ejecute duración
+    if (audio.readyState > 0) {
+        displayDuration();
+        setSliderMax();
+        displayBufferedAmount();
+        audio.play();
+    } else {
+        audio.addEventListener('loadedmetadata', () => {
+            displayDuration();
+            setSliderMax();
+            displayBufferedAmount();
+            audio.play();
+        });
+    }
+    return false;
+};
+
+
+
