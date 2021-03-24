3c31e28a2007f2b186ecf6d7168a4900879816e2
Daniel Vega <98dvega@gmail.com>
2021-03-24 06:25:49 -0500

    Movable and toggeable functionality done.

--- player.js
+++ player.js
@@ -520,36 +520,97 @@ cool.oncontextmenu = ()=>{
 
 //MOVABLE || DRAGGABLE 
 var draggableDiv = document.querySelector("#draggableDiv");
+
+
+var PADDING = 1;
+
+var rect;
+var viewport = {
+  bottom: 0,
+  left: 0,
+  right:0,
+  top: 0
+}
+
+//Make the DIV element draggagle:
 dragElement(draggableDiv);
+dragElement(title);
 
-function dragElement(elmnt){
-    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-    if(elmnt){
-        elmnt.onmousedown = dragMouseDown;
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (document.getElementById(elmnt.id + "header")) {
+    /* if present, the header is where you move the DIV from:*/
+    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+  } else {
+    /* otherwise, move the DIV from anywhere inside the DIV:*/
+    elmnt.onmousedown = dragMouseDown;
+  }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    
+    // store the current viewport and element dimensions when a drag starts
+    rect = elmnt.getBoundingClientRect();
+    viewport.bottom = window.innerHeight - PADDING;
+    viewport.left = PADDING;
+    viewport.right = window.innerWidth - PADDING;
+    viewport.top = PADDING;
+    
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    
+    // check to make sure the element will be within our viewport boundary
+    var newLeft = elmnt.offsetLeft - pos1;
+    var newTop = elmnt.offsetTop - pos2;
+
+    if (newLeft < viewport.left
+        || newTop < viewport.top
+        || newLeft + rect.width > viewport.right
+        || newTop + rect.height > viewport.bottom
+    ) {
+    	// the element will hit the boundary, do nothing...
+    } else {
+      // set the element's new position:
+      elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+      elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
     }
+  }
 
-    function dragMouseDown(e){
-        e = e || window.event;
-        e.preventDefault;
-        pos3 = e.clientX;
-        pos4 = e.clientY;
-        document.onmouseup = closeDragElement;
-        document.onmousemove = elementDrag;
+  function closeDragElement() {
+    /* stop moving when mouse button is released:*/
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
+
+//Erase/recreate div
+let divKills = 0;
+function divKiller(){
+    if(divKills == 0){
+        details.appendChild(audioPlayerContainer);
+        divKills++;
+        draggableDiv.style.width = "450px";
     }
-
-    function elementDrag(e){
-        e = e || window.event;
-        e.preventDefault();
-        pos1 = pos3 - e.clientX;
-        pos2 = pos4 - e.clientY;
-        pos3 = e.clientX;
-        pos4 = e.clientY;
-        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-    }
-
-    function closeDragElement(){
-        document.onmouseup = null;
-        document.onmousemove = null;
+    else{
+        audioPlayerContainer.remove();
+        draggableDiv.style.width = "150px";
+        divKills--;
     }
 }
+
+details.addEventListener('toggle',divKiller);
+
+document.oncontextmenu = () => {return false};
